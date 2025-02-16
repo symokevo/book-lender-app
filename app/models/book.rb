c75fb719bfd82b3fcf4f42d65396a1302b924caf
Simon_R <symokevo@gmail.com>
2025-02-16 15:58:37 +0300

    Update books controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,6 @@
 class Book < ApplicationRecord
   validates :title, :author, :isbn, presence: true
   validates :isbn, uniqueness: true
-  has_many :borrowings
+  has_many :borrowings, dependent: :destroy
   has_many :users, through: :borrowings
 end
